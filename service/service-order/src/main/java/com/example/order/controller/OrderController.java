b10f96a25cf739deed9b9bd60f34031ff49165bc
lzFATE <162133946+lzFATE@users.noreply.github.com>
2025-03-10 16:49:38 +0800

    第二次提交

--- service/service-order/src/main/java/com/example/order/controller/OrderController.java
+++ service/service-order/src/main/java/com/example/order/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.example.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.order.bean.Order;
 import com.example.order.properties.OrderProperties;
 import com.example.order.service.OrderService;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @Slf4j
 // @RefreshScope 自动刷新配置
@@ -43,10 +47,21 @@ public class OrderController {
 
     // 创建订单
     @GetMapping("/seckill")
-    public Order seckill(@RequestParam("userId") Long userId,
-                             @RequestParam("productId") Long productId) {
+    @SentinelResource(value = "seckill-order", fallback = "seckillFallback")
+    public Order seckill(@RequestParam(value = "userId",required = false) Long userId,
+                             @RequestParam(value = "productId",defaultValue = "999") Long productId) {
         Order order = ordreService.createOrder(userId, productId);
         order.setId(Long.MAX_VALUE);
+        return order;
+    }
+
+    public Order seckillFallback(Long userId, Long productId, Throwable exception) {
+        System.out.println("seckillFallback方法被调用");
+
+        Order order = new Order();
+        order.setId(productId);
+        order.setUserId(userId);
+        order.setAddress("异常信息：" + exception.getClass());
         return order;
     }
 
--- service/service-product/src/main/java/com/example/product/controller/ProductController.java
+++ service/service-product/src/main/java/com/example/product/controller/ProductController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class ProductController {
 
@@ -23,6 +25,12 @@ public class ProductController {
         String header = request.getHeader("X-Token");
         System.out.println("hello = " + "XToken: " + header);
         Product product = productService.getProductById(productId);
+//        int i = 1 / 0;
+//        try {
+//            TimeUnit.SECONDS.sleep(2);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
         return product;
     }
 
